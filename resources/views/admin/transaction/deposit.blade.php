376a1023a5ca6232b12d7f628837971825e95eae
Caleb Joseph <iamleblib@Calebs-MacBook-Pro.local>
2021-11-12 10:28:56 +0100

    fixes

--- resources/views/admin/transaction/deposit.blade.php
+++ resources/views/admin/transaction/deposit.blade.php
@@ -206,7 +206,7 @@
                                             <td data-order="2020-09-11T15:15:00+01:00">{{ $deposit->created_at->toFormattedDateString() }}</td>
                                             <!--end::Date=-->
                                             <!--begin::Action=-->
-                                            <td class="text-end">
+                                            <td>
                                                 @if($deposit->status == 'processed')
 
                                                 <a href="#" class="btn btn-sm btn-light btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
@@ -242,7 +242,7 @@
                                                         <!--end::Svg Icon-->
                                                     </a>
                                                     <!--begin::Menu-->
-                                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-9" data-kt-menu="true">
+                                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4" data-kt-menu="true">
                                                         <!--begin::Menu item-->
                                                         <div class="menu-item px-3">
                                                             <a data-bs-toggle="modal" data-bs-target="#approve_{{ $deposit->id }}" class="menu-link px-3">Approve</a>
