8c4b62b0dd96d594ca0691c2520e553a88604620
Caleb Joseph <iamleblib@Calebs-MacBook-Pro.local>
2021-11-12 10:20:33 +0100

    fixes

--- resources/views/admin/transaction/deposit.blade.php
+++ resources/views/admin/transaction/deposit.blade.php
@@ -242,7 +242,7 @@
                                                         <!--end::Svg Icon-->
                                                     </a>
                                                     <!--begin::Menu-->
-                                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4 m-5" data-kt-menu="true">
+                                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4" data-kt-menu="true">
                                                         <!--begin::Menu item-->
                                                         <div class="menu-item px-3">
                                                             <a data-bs-toggle="modal" data-bs-target="#approve_{{ $deposit->id }}" class="menu-link px-3">Approve</a>
@@ -254,6 +254,13 @@
                                                         </div>
                                                         <!--end::Menu item-->
                                                     </div>
+                                                    <br>
+                                                    <br>
+                                                    <br>
+
+                                                &nbsp;
+                                                &nbsp;
+                                                &nbsp;
                                                     <!--end::Menu-->
                                                 @elseif($deposit->status == 'declined')
                                                     <a href="#" class="btn btn-sm btn-light btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
