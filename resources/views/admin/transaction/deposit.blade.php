35c5ce151a10e549627e5dc7dfa2e0227e89f6f7
Caleb Joseph <iamleblib@Calebs-MacBook-Pro.local>
2021-11-12 08:43:04 +0100

    fixes

--- resources/views/admin/transaction/deposit.blade.php
+++ resources/views/admin/transaction/deposit.blade.php
@@ -241,6 +241,7 @@
                                                      </span>
                                                         <!--end::Svg Icon-->
                                                     </a>
+{{--                                                fake out--}}
                                                     <!--begin::Menu-->
                                                     <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4" data-kt-menu="true">
                                                         <!--begin::Menu item-->
