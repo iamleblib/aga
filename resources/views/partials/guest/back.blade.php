d3a9ce2bf24e53ff883961c3fab9aa38c1b67552
Prince Xrae <xraecity.xcl@yahoo.com>
2021-11-12 08:21:50 +0100

    Erro anotherr

--- resources/views/partials/guest/back.blade.php
+++ resources/views/partials/guest/back.blade.php
@@ -1,3 +1,9 @@
+<script>
+    function goBack() {
+        window.history.back();
+    }
+</script>
+
 <div class="d-flex align-items-center py-3 py-md-1">
     <a href="#" class="btn btn-bg-white btn-active-color-primary" onclick="goBack()"><i class="fa fa-arrow-left"> </i>Back</a>
     <!--end::Button-->
@@ -7,9 +13,4 @@
     <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FS8GGP" height="0" width="0" style="display:none;visibility:hidden"></iframe>
 </noscript>
 
-<script>
-    function goBack() {
-        window.history.back();
-    }
-</script>
 <!--End::Google Tag Manager (noscript) -->
