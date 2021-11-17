8edefa77e57027eb8328a2adc99ca8e0fcd44c40
Caleb Joseph <calebbjoseph@gmail.com>
2021-11-17 08:27:17 +0100

    Fixed bugs

--- resources/views/guest/profile/billing.blade.php
+++ resources/views/guest/profile/billing.blade.php
@@ -23,18 +23,15 @@
                                     <div class="d-flex align-items-center fs-4 fw-bolder mb-5">{{ $wallet->name }}</div>
                                     <!--end::Owner-->
                                     <!--begin::Wrapper-->
-                                    <div class="d-flex align-items-center">
+                                    <div class="align-items-center">
                                         <!--begin::Icon-->
                                         @if($wallet->name == "Bitcoin (BTC)")
-                                            <img src="{{ asset('backend/assets/media/icons/bitcoin.png') }}" width="10%" alt="" class="me-4" />
+                                            <img src="{{ asset('backend/assets/media/icons/bitcoin.png') }}" width="10%" alt="" class="m-4" />
                                         @elseif($wallet->name == "Etherium (ETH)")
-                                            <img src="{{ asset('backend/assets/media/icons/etherium.png') }}" width="10%" alt="" class="me-4" />
+                                            <img src="{{ asset('backend/assets/media/icons/etherium.png') }}" width="10%" alt="" class="m-4" />
                                         @elseif($wallet->name == "USDC (USDC)")
-                                            <img src="{{ asset('backend/assets/media/icons/usdc.png') }}" width="10%" alt="" class="me-4" />
+                                            <img src="{{ asset('backend/assets/media/icons/usdc.png') }}" width="10%" alt="" class="m-4" />
                                         @endif
-
-                                        <!--end::Icon-->
-                                        <!--begin::Details-->
                                         <div>
                                             <div class="fs-4 fw-bolder">Wallet address</div>
                                             <div class="fs-6 fw-bold text-gray-400">{{ $wallet->address }}</div>
